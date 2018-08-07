b623e4dd8d2c94eef4600f4c79baa446e77700fa
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 18:11:09 +0300

    you're a fucking wizard harry

--- exercises/app.py
+++ exercises/app.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template("index.html")
+	players = ["Messi", "Griezmann", "Mo Salah", "Buffon", "Mbappe"] 
+	return render_template(
+		"index.html",
+		players=players,
+		likes_same_sport = True)
 
 if __name__ == '__main__':
    app.run(debug = True)
